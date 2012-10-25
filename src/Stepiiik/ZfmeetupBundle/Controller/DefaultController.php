724bdb13c7115a100b9ef6814aa84456e9b2d76e
Stepan Zikmund <stepan.zikmund@gmail.com>
2012-10-25 17:06:13 +0200

    Controller with @template

--- src/Stepiiik/ZfmeetupBundle/Controller/DefaultController.php
+++ src/Stepiiik/ZfmeetupBundle/Controller/DefaultController.php
@@ -4,10 +4,15 @@ namespace Stepiiik\ZfmeetupBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 class DefaultController extends Controller
 {
+	/**
+	 * @Template()
+	 */
     public function indexAction($name)
     {
-        return $this->render('StepiiikZfmeetupBundle:Default:index.html.twig', array('name' => $name));
+        return array('name' => $name);
     }
 }
