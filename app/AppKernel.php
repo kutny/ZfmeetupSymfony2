5b5ab2a2f0224c3125d16562a01778d268376fd8
Jiri Koutny <jirkakoutny@gmail.com>
2013-06-13 10:45:41 +0200

    Symfony updated to version 2.3

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,10 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new Stepiiik\ZfmeetupBundle\StepiiikZfmeetupBundle(),
+            new Stepiiik\ZfmeetupBundle\StepiiikZfmeetupBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
