5d8f5836d4a0d0436965c3857485e841a400d0f7
Jiri Koutny <jirkakoutny@gmail.com>
2013-06-13 15:10:40 +0200

    Getters & setters cleared

--- src/Stepiiik/ZfmeetupBundle/Entity/Album.php
+++ src/Stepiiik/ZfmeetupBundle/Entity/Album.php
@@ -36,59 +36,34 @@ class Album
      */
     private $id;
 
-    /**
-     * Set artist
-     *
-     * @param string $artist
-     * @return Album
-     */
     public function setArtist($artist)
     {
         $this->artist = $artist;
-    
-        return $this;
     }
 
-    /**
-     * Get artist
-     *
-     * @return string 
-     */
     public function getArtist()
     {
         return $this->artist;
     }
 
-    /**
-     * Set title
-     *
-     * @param string $title
-     * @return Album
-     */
     public function setTitle($title)
     {
         $this->title = $title;
-    
-        return $this;
     }
 
-    /**
-     * Get title
-     *
-     * @return string 
-     */
     public function getTitle()
     {
         return $this->title;
     }
 
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
     public function getId()
     {
         return $this->id;
     }
+
 }
