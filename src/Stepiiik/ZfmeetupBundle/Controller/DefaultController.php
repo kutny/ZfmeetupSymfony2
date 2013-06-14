04a42332c2f4bec7640e95b4fe2927d3be8546e7
Jiri Koutny <jirkakoutny@gmail.com>
2013-06-14 15:02:31 +0200

    User should be redirected from / to /album when authenticated

--- src/Stepiiik/ZfmeetupBundle/Controller/DefaultController.php
+++ src/Stepiiik/ZfmeetupBundle/Controller/DefaultController.php
@@ -4,14 +4,40 @@ namespace Stepiiik\ZfmeetupBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Router;
+use Symfony\Component\Security\Core\SecurityContext;
 
 /**
  * @Route(service = "controller.default_controller")
  */
 class DefaultController
 {
+    private $router;
+    private $securityContext;
+
+    public function __construct(Router $router, SecurityContext $securityContext) {
+        $this->router = $router;
+        $this->securityContext = $securityContext;
+    }
+
+    /**
+     * @Route("/", name="route.homepage")
+     */
+    public function homepageAction()
+    {
+        $userIsAuthenticated = $this->securityContext->getToken()->isAuthenticated();
+
+        if ($userIsAuthenticated) {
+            return new RedirectResponse($this->router->generate('route.album'));
+        }
+        else {
+            return new RedirectResponse($this->router->generate('route.login'));
+        }
+    }
+
 	/**
-     * @Route("/hello/{name}", name="route.stepiiik_zfmeetup_homepage")
+     * @Route("/hello/{name}", name="route.hello")
 	 * @Template()
 	 */
     public function helloAction($name)
